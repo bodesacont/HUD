7e0aa685c65db86040ffe3a1ac210b464da3761f
bodesacont <133894428+bodesacont@users.noreply.github.com>
2025-09-30 22:53:50 -0400

    Create app.js

---
+++ app.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import AppNav from './components/AppNav';
+import CalendarTodo from './components/CalendarTodo';
+import Weather from './components/Weather';
+import MusicClock from './components/MusicClock';
+import './styles.css';
+
+function App() {
+  return (
+    <div className="hud-background">
+      <AppNav />
+      <div className="hud-layout">
+        <CalendarTodo />
+        <div>
+          <Weather />
+          <MusicClock />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
