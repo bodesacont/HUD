0fd0c34b4798692065a265eb8c85dc6432febc79
bodesacont <133894428+bodesacont@users.noreply.github.com>
2025-09-30 22:58:04 -0400

    Create Weather.js

---
+++ src/components/Weather.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Weather.css";
+
+const Weather = () => (
+  <div className="widget weather-widget">
+    <div className="widget-title">Weather</div>
+    <div>Sunny, 75°F</div>
+    <div>Tomorrow: Cloudy, 70°F</div>
+  </div>
+);
+
+export default Weather;
