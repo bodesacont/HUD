ab4c00512ab139c5e6d84e8f67a4c2bdd536c54b
bodesacont <133894428+bodesacont@users.noreply.github.com>
2025-09-30 22:57:25 -0400

    Create CalanderTodo.js

---
+++ src/components/CalanderTodo.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./CalendarTodo.css";
+
+const CalendarTodo = () => (
+  <div className="widget calendar-todo">
+    <div className="widget-title">Calendar with to do</div>
+    <ul>
+      <li>Meeting at 10am</li>
+      <li>Lunch with Sam</li>
+      <li>Finish HUD UI</li>
+    </ul>
+  </div>
+);
+
+export default CalendarTodo;
