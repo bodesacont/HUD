650b75650818fbf0704f01fd8cc284a5d052030e
bodesacont <133894428+bodesacont@users.noreply.github.com>
2025-09-30 22:56:48 -0400

    Create AppNav.js

---
+++ src/components/AppNav.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./AppNav.css";
+
+const AppNav = () => (
+  <div className="app-nav">
+    <div className="circle app-circle">app 1</div>
+    <div className="circle app-circle main">apps</div>
+    <div className="circle app-circle">app 3</div>
+    <div className="nav-arrow">&#8596;</div>
+  </div>
+);
+
+export default AppNav;
